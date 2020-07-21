fd012ef5f5a44732259ddda406dc87d23c8d9955
SEXYJungmin <jmdev@kakao.com>
2020-07-22 01:00:58 +0900

    2020/07/22 : (실습) JSX 코드 작성해보기

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,12 +4,27 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
+/* ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
-);
+); */
+
+const formatName = function(name) {
+  return name.firstName + " " + name.lastName;
+}
+
+const name = {
+  firstName : "Jungmin",
+  lastName : "Mun",
+};
+
+const element = <h1>Hello, {formatName(name)}!</h1>;
+
+ReactDOM.render(
+  element,
+  document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
